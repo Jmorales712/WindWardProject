c9cdabdc9baf9d1c1e2d39f27d5051deb417005a
Jmorales712 <110206323+Jmorales712@users.noreply.github.com>
2026-06-19 14:50:09 -0400

    Update Home.java

--- src/test/java/Pages/Home.java
+++ src/test/java/Pages/Home.java
@@ -4,6 +4,8 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class Home {
 
 
